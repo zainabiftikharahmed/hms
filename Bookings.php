1bafc5c5aba402fa8419ddbbfef8bcc8a28a51d5
Zainab Iftikhar <zainabiftikhar87@gmail.com>
2018-04-18 19:41:19 +0500

    make it better

--- Bookings.php
+++
@@ -1,8 +0,0 @@
-<!-- <% if(session.getAttribute("Type")==null){ %>
-   <jsp:include page="Header.jsp" />
-<%}else if((Integer)session.getAttribute("Type")==3){%>
-   <jsp:include page="UserHeader.jsp" />
-<%}%> -->
-
-<?php require ('Header.php') ?>
-<?php require ('Footer.php') ?>
